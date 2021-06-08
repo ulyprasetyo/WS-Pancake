de726a0abbd00a35c735eb00fb03a38c3f951382
ulyprasetyo <47936636+ulyprasetyo@users.noreply.github.com>
2021-06-08 09:55:05 +0700

    Update api.php

--- api.php
+++ api.php
@@ -2,17 +2,17 @@
 
 function apiNewTokenAccess(){
 	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_URL,"https://wstraining.bkn.go.id/oauth/token");
+	curl_setopt($curl, CURLOPT_URL,"https://wstraining.bkn.go.id/oauth/token"); //ini bukan server production
 	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST"); 
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_USERPWD, "kemenperintr:tyegt6789");
-	curl_setopt($curl, CURLOPT_POSTFIELDS,"client_id=kemenperintr&grant_type=client_credentials");
+	curl_setopt($curl, CURLOPT_USERPWD, "user:pass"); //diubah sesuai user dan pass yang didapat
+	curl_setopt($curl, CURLOPT_POSTFIELDS,"client_id=user&grant_type=client");
 	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded','origin: http://localhost:20000'));
 
 
-	// receive server response ...
+	// server response ...
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 
@@ -22,6 +22,8 @@ function apiNewTokenAccess(){
 	}
 	else
 	{
+		
+// file token akan dibuat otomatis
 		$obj = json_decode($jsondata, true);
 		if(isset($obj['access_token'])){
 			$token_file = fopen("token-key.txt", "w") or die("Unable to open file!");
@@ -33,6 +35,8 @@ function apiNewTokenAccess(){
 	
 	curl_close ($curl);
 }
+
+// GET data setelah token berhasil dibuat
 
 function apiResult( $url = ''){
 	$token_file = fopen("token-key.txt", "r") or die("Unable to open file!");
@@ -54,7 +58,7 @@ function apiResult( $url = ''){
 	));
 
 
-	// receive server response ...
+	// server response ...
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	
 	return curl_exec($curl);
